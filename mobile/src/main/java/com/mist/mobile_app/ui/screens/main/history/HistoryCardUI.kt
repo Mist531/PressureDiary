e486699d5247ee65b4d987ca3cc2602bc2e54e2a
nikita lukin <nik03luk@mail.ru>
2024-05-13 23:58:20 +0300

    add HistoryLineInfoStatus

--- mobile/src/main/java/com/mist/mobile_app/ui/screens/main/history/HistoryCardUI.kt
+++ mobile/src/main/java/com/mist/mobile_app/ui/screens/main/history/HistoryCardUI.kt
@@ -7,18 +7,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -32,6 +36,50 @@ import com.mist.mobile_app.ui.theme.PDTheme
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.UUID
+
+enum class HistoryStatus(
+    val color: Color,
+    val id: Int
+) {
+    BLUE(
+        color = Color(0xFF16C8EF),
+        id = 0
+    ),
+    GREEN(
+        color = Color(0xFF23F15D),
+        id = 1
+    ),
+    ORANGE(
+        color = Color(0xFFFF8934),
+        id = 2
+    ),
+    RED(
+        color = Color(0xFFF92D2D),
+        id = 3
+    ),
+    DEFAULT(
+        color = Color(0xFFE7E7E7),
+        id = 4
+    );
+
+    companion object {
+        val countLine = 0..3
+    }
+}
+
+//TODO: replace with settings value
+fun PressureRecordModel.getInfoColor() = when {
+    this.diastolic <= 60 || this.systolic <= 90 -> HistoryStatus.BLUE
+
+    (this.diastolic in 61..80 && this.systolic in 91..120) -> HistoryStatus.GREEN
+
+    (this.diastolic in 81..90 && this.systolic in 121..140) -> HistoryStatus.ORANGE
+
+    (this.diastolic >= 91 || this.systolic >= 141) -> HistoryStatus.RED
+
+    else -> HistoryStatus.DEFAULT
+}
+
 
 @Composable
 fun HistoryCard(
@@ -63,7 +111,7 @@ fun HistoryCard(
     ) {
         HistoryCardItemTop(
             modifier = Modifier,
-            dateTimeRecord = record.dateTimeRecord
+            record = record
         )
         HistoryCardItemCenter(
             modifier = Modifier,
@@ -81,7 +129,7 @@ fun HistoryCard(
 @Composable
 fun HistoryCardItemTop(
     modifier: Modifier = Modifier,
-    dateTimeRecord: LocalDateTime,
+    record: PressureRecordModel,
 ) {
     Row(
         modifier = modifier
@@ -91,9 +139,48 @@ fun HistoryCardItemTop(
         horizontalArrangement = Arrangement.Start
     ) {
         HistoryCardItemTimeInfo(
-            dateTimeRecord = dateTimeRecord
+            dateTimeRecord = record.dateTimeRecord
         )
-        //TODO line info status record
+        HistoryLineInfoStatus(
+            modifier = Modifier
+                .padding(
+                    start = 32.dp
+                ),
+            record = record,
+        )
+    }
+}
+
+@Composable
+fun HistoryLineInfoStatus(
+    modifier: Modifier = Modifier,
+    record: PressureRecordModel,
+) {
+    val selectColor = remember(record) {
+        record.getInfoColor()
+    }
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        HistoryStatus.countLine.forEach { index ->
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .height(6.dp)
+                    .weight(1f),
+                progress = remember(selectColor) {
+                    if (selectColor.id == index) {
+                        1f
+                    } else
+                        0f
+                },
+                color = selectColor.color,
+                trackColor = HistoryStatus.DEFAULT.color,
+                strokeCap = StrokeCap.Round
+            )
+        }
     }
 }
 
@@ -283,5 +370,44 @@ fun PreviewHistoryCard() {
                 note = "",
             )
         )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHistoryLineInfoStatus() {
+    PDTheme {
+        Column {
+            HistoryLineInfoStatus(
+                record = PressureRecordModel(
+                    pressureRecordUUID = UUID.randomUUID(),
+                    dateTimeRecord = LocalDateTime.now(),
+                    systolic = 120,
+                    diastolic = 80,
+                    pulse = 80,
+                    note = ""
+                )
+            )
+            HistoryLineInfoStatus(
+                record = PressureRecordModel(
+                    pressureRecordUUID = UUID.randomUUID(),
+                    dateTimeRecord = LocalDateTime.now(),
+                    systolic = 130,
+                    diastolic = 90,
+                    pulse = 80,
+                    note = ""
+                )
+            )
+            HistoryLineInfoStatus(
+                record = PressureRecordModel(
+                    pressureRecordUUID = UUID.randomUUID(),
+                    dateTimeRecord = LocalDateTime.now(),
+                    systolic = 50,
+                    diastolic = 100,
+                    pulse = 80,
+                    note = ""
+                )
+            )
+        }
     }
 }
